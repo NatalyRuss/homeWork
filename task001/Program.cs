8a2a4a48d740521c00cfee3743cb473c02752d66
TATASHA <n.bazhutina20@gmail.com>
2023-06-04 13:20:56 +0300

    дописали программу до конца

--- task001/Program.cs
+++ task001/Program.cs
@@ -38,3 +38,31 @@ for (int i = 0; i <= array.Length - 1; i++)
 PrintArray(array);
 Console.Write("→ ");
 PrintArray(newArray);
+
+void Commands()
+{
+    Console.WriteLine();
+    Console.WriteLine("СПИСОК КОМАНД:");
+    Console.WriteLine("1 – использовать массив: [“Hello”, “2”, “world”, “:-)”]");
+    Console.WriteLine("2 – использовать массив: [“1234”, “1567”, “-2”, “computer science”]");
+    Console.WriteLine("3 – использовать массив: [“Russia”, “Denmark”, “Kazan”]");
+    Console.WriteLine();
+}
+
+// Функция ввода
+string ReadInput(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine();
+}
+
+//  Функция вывода массива в терминал
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"“{array[i]}”, ");
+    }
+    Console.Write("] ");
+}
